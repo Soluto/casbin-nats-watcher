c04bab22d3d5b9134c44cacdd637fa666852004a
Alex <AlexanderFux83@gmail.com>
2018-02-06 15:30:40 +0200

    watcher implementation added (#1)
    watcher implementation added

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # casbin-nats-watcher
-Casbin watcher implementation with Nats.io
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Build Status](https://travis-ci.org/Soluto/casbin-nats-watcher.svg?branch=master)](https://travis-ci.org/Soluto/casbin-nats-watcher)
+[![Coverage Status](https://coveralls.io/repos/github/Soluto/casbin-nats-watcher/badge.svg?branch=master)](https://coveralls.io/github/Soluto/casbin-nats-watcher?branch=master)
+[![Godoc](https://godoc.org/github.com/Soluto/casbin-nats-watcher?status.svg)](https://godoc.org/github.com/Soluto/casbin-nats-watcher)
+
+[Casbin](https://github.com/casbin/casbin) watcher implementation with Nats.io
+
+## Installation
+
+go get github.com/Soluto/casbin-nats-watcher
+
+## Usage
+```go
+import (
+	natswatcher "github.com/Soluto/casbin-nats-watcher"
+	"github.com/casbin/casbin"
+)
+
+func main() {
+	watcher, _ := natswatcher.NewWatcher("http://...", "my-policy-subject")
+
+	enforcerer := casbin.NewSyncedEnforcer("", "")
+	enforcerer.SetWatcher(watcher)
+}
+```
+
+## Connected pojects
+- [Casbin](https://github.com/casbin/casbin)
+- [Nats.io](https://github.com/nats-io/go-nats)
+
+
+## Additional Usage Examples
+
+For real-world example visit [Tweek](https://github.com/Soluto/tweek)
+
+## License
+
+This project is under MIT License. See the [LICENSE](LICENSE) file for the full license text.
---
+++ watcher_test.go
@@ -0,0 +1,125 @@
+package natswatcher
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/casbin/casbin"
+	gnatsd "github.com/nats-io/gnatsd/test"
+	"github.com/nats-io/go-nats"
+)
+
+func TestWatcher(t *testing.T) {
+	// Setup nats server
+	s := gnatsd.RunDefaultServer()
+	defer s.Shutdown()
+
+	natsEndpoint := fmt.Sprintf("nats://localhost:%d", nats.DefaultPort)
+	natsSubject := "casbin-policy-updated-subject"
+
+	updaterCh := make(chan string, 1)
+	listenerCh := make(chan string, 1)
+
+	// updater represents the Casbin enforcer instance that changes the policy in DB.
+	// Use the endpoint of nats as parameter.
+	updater, err := NewWatcher(natsEndpoint, natsSubject)
+	if err != nil {
+		t.Fatal("Failed to create updater")
+	}
+	updater.SetUpdateCallback(func(msg string) {
+		updaterCh <- "updater"
+	})
+
+	// listener represents any other Casbin enforcer instance that watches the change of policy in DB.
+	listener, err := NewWatcher(natsEndpoint, natsSubject)
+	if err != nil {
+		t.Fatal("Failed to create listener")
+	}
+
+	// listener should set a callback that gets called when policy changes.
+	err = listener.SetUpdateCallback(func(msg string) {
+		listenerCh <- "listener"
+	})
+	if err != nil {
+		t.Fatal("Failed to set listener callback")
+	}
+
+	// updater changes the policy, and sends the notifications.
+	err = updater.Update()
+	if err != nil {
+		t.Fatal("The updater failed to send Update")
+	}
+
+	// Validate that listener received message
+	var updaterReceived bool
+	var listenerReceived bool
+	for {
+		select {
+		case res := <-listenerCh:
+			if res != "listener" {
+				t.Fatalf("Message from unknown source: %v", res)
+				break
+			}
+			listenerReceived = true
+		case res := <-updaterCh:
+			if res != "updater" {
+				t.Fatalf("Message from unknown source: %v", res)
+				break
+			}
+			updaterReceived = true
+		case <-time.After(time.Second * 10):
+			t.Fatal("Updater or listener didn't received message in time")
+			break
+		}
+		if updaterReceived && listenerReceived {
+			close(listenerCh)
+			close(updaterCh)
+			break
+		}
+	}
+
+}
+
+func TestWithEnforcer(t *testing.T) {
+	// Setup nats server
+	s := gnatsd.RunDefaultServer()
+	defer s.Shutdown()
+
+	natsEndpoint := fmt.Sprintf("nats://localhost:%d", nats.DefaultPort)
+	natsSubject := "casbin-policy-updated-subject"
+	cannel := make(chan string, 1)
+
+	// Initialize the watcher.
+	// Use the endpoint of etcd cluster as parameter.
+	w, err := NewWatcher(natsEndpoint, natsSubject)
+	if err != nil {
+		t.Fatal("Failed to create updater")
+	}
+
+	// Initialize the enforcer.
+	e := casbin.NewEnforcer("examples/rbac_model.conf", "examples/rbac_policy.csv")
+
+	// Set the watcher for the enforcer.
+	e.SetWatcher(w)
+
+	// By default, the watcher's callback is automatically set to the
+	// enforcer's LoadPolicy() in the SetWatcher() call.
+	// We can change it by explicitly setting a callback.
+	w.SetUpdateCallback(func(msg string) {
+		cannel <- "enforcerer"
+	})
+
+	// Update the policy to test the effect.
+	e.SavePolicy()
+
+	// Validate that listener received message
+	select {
+	case res := <-cannel:
+		if res != "enforcerer" {
+			t.Fatalf("Got unexpected message :%v", res)
+		}
+	case <-time.After(time.Second * 10):
+		t.Fatal("The enforcerer didn't send message in time")
+	}
+}
